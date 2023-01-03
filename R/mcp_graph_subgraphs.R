1e58f8d0b99dfb96022fad4ea2938eb25a30f740
Ethan Brockmann <ethan.brockmann@gilead.com>
2023-01-03 11:31:51 -0600

    Attempts adding GitHub CI stuff from {gsm}, tweaks README, and updates description/license

--- R/mcp_graph_subgraphs.R
+++ R/mcp_graph_subgraphs.R
@@ -2,41 +2,41 @@
 #'
 #' @export
 calc_subgraphs <- function(g) {
-	UseMethod("calc_subgraphs")
+  UseMethod("calc_subgraphs")
 }
 
 #' @export
 calc_subgraphs.mcp_graph <- function(g) {
-	list_subgraphs <- lapply(
-		wgt_powerset(g),
-		function(x, y) as.integer(y %in% x),
-		g
-	)
+  list_subgraphs <- lapply(
+    wgt_powerset(g),
+    function(x, y) as.integer(y %in% x),
+    g
+  )
 
-	list_wgt_subgraphs <- lapply(
-		list_subgraphs,
-		function(h, w, g) wgt_delete_nodes(h, w, g),
-		get_hyp_weights(g),
-		get_trns_weights(g)
-	)
+  list_wgt_subgraphs <- lapply(
+    list_subgraphs,
+    function(h, w, g) wgt_delete_nodes(h, w, g),
+    get_hyp_weights(g),
+    get_trns_weights(g)
+  )
 
-	mtx_wgt_subgraphs <- do.call(rbind, list_wgt_subgraphs)
+  mtx_wgt_subgraphs <- do.call(rbind, list_wgt_subgraphs)
 
-	colnames(mtx_wgt_subgraphs) <- names(g)
+  colnames(mtx_wgt_subgraphs) <- names(g)
 
-	attr(g, "subgraphs") <- mtx_wgt_subgraphs
+  attr(g, "subgraphs") <- mtx_wgt_subgraphs
 
-	g
+  g
 }
 
 #' Get the weighted subgraphs of a multiple comparison procedure graph
 #'
 #' @export
 get_subgraphs <- function(g) {
-	UseMethod("get_subgraphs")
+  UseMethod("get_subgraphs")
 }
 
 #' @export
 get_subgraphs.mcp_graph <- function(g) {
-	attr(g, "subgraphs")
+  attr(g, "subgraphs")
 }
