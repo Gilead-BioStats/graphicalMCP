04133f0b52719866690f258f91fd79020e622bde
ethan.brockmann <ethan.brockmann@atorusresearch.com>
2023-01-10 20:23:22 +0000

    Updated print function

--- R/graph_print.R
+++ R/graph_print.R
@@ -1,25 +1,16 @@
-print_title <- function() {
-  "An MCP graph" # TODO: Explore {crayon} for using bold or colored text
-}
-
 print_hypotheses <- function(graph) {
   hypotheses_text <- paste(
     formatC(
       names(graph$hypotheses),
       width = max(nchar(names(graph$hypotheses)))
     ),
-    ": (",
+    ": ",
     formatC(graph$hypotheses, digits = 4, format = "f"),
-    ")",
     sep = "",
     collapse = "\n"
   )
 
-  paste(
-    "--- Hypothesis weights ---",
-    hypotheses_text,
-    sep = "\n"
-  )
+  hypotheses_text
 }
 
 print_transitions <- function(graph) {
@@ -28,36 +19,28 @@ print_transitions <- function(graph) {
     digits = 4,
     format = "f"
   )
-  diag(transitions) <- rep("  --  ", ncol(transitions))
 
   transitions_text <- data.frame(transitions, check.names = FALSE)
 
   transitions_text
 }
 
-#' S3 print method for class 'initial_graph'
+#' S3 print method for the class 'initial_graph'
 #'
 #' A printed `initial_graph` displays a header stating what the object is, the
 #' hypothesis weights, and the transition weights.
 #'
 #' @param x An object of class `initial_graph` to print
-#'
 #' @param ... Other values passed on to other methods (currently unused)
-#'
 #' @export
 print.initial_graph <- function(x, ...) {
-  if (length(x$hypotheses) == 0) {
-    cat("An empty graph\n")
-  } else {
-    print_graph <- paste(
-      print_title(),
-      print_hypotheses(x),
-      sep = "\n\n"
-    )
+  cat("An initial graph\n\n")
 
-    cat(print_graph, "", sep = "\n")
+  cat("--- Hypothesis weights ---\n")
 
-    cat("--- Transition weights ---\n")
-    print(print_transitions(x))
-  }
+  cat(print_hypotheses(x), "", sep = "\n")
+
+  cat("--- Transition weights ---\n")
+
+  print(print_transitions(x))
 }
