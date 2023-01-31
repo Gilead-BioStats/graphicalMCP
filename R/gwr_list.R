dc5c9e9995b94f35aef5e808972e2be5a6965c29
EeethB <ethanbrockmann@gmail.com>
2023-01-31 19:55:49 +0000

    Updates `gwr_list()` to use a faster implementation, causing it to slightly outperform the recursive implementation in speed

--- R/gwr_list.R
+++ R/gwr_list.R
@@ -1,42 +1,130 @@
-powerset_parents_ <- function(n) {
-  ps <- expand.grid(rep(list(1:0), n))[-2 ^ n,]
+powerset_parents_fast <- function(n) {
+  len <- 2 ^ n
+
+  parents <- c(NA, do.call(c, lapply(2 ^ (seq_len(n) - 1), seq_len)))[-2 ^ n]
+
+  delete <- c(NA, rep(seq_len(n), 2 ^ (seq_len(n) - 1)))[-2 ^ n]
+
+  list(parents = parents, delete = delete)
+}
+
+# A bit of time can be saved by pre-computing the child/parent relationship for
+# the first n graph sizes. These relationships can then be loaded, rather than
+# re-calculated
+save_powerset_parents_fast <- function(n_start, n_end) {
+  lapply(
+    n_start:n_end,
+    function(n) {
+      ps_p <- powerset_parents_fast(n)
+      saveRDS(ps_p, paste0("./perf-tests/data/", n, ".rds"))
+    }
+  )
+}
+
+# Uses method of finding the last 0 and replacing it with 1 to get to the parent
+# It turns out, this yields a pattern that is simple to reproduce with formulas,
+# as is done in `powerset_parents_fast()` above
+powerset_parents <- function(n) {
+  ps <- expand.grid(rep(list(1:0), n))[-2^n, ]
 
   ps_children <- apply(ps, 1, paste, collapse = "", simplify = TRUE)
 
-  ps_parents <- sub("0", "1", ps_children)
+  ps_parents <- sub("0([^0]*)$", "1\\1", ps_children)
 
-  to_delete <- apply(ps, 1, \(h) which(h == 0)[1])
+  to_delete <- apply(
+    ps,
+    1,
+    function(h) {
+      count_0 <- sum(h == 0)
+      if (count_0 == 0) return(NA)
+      which(h == 0)[count_0]
+    }
+  )
 
   parent_indices <- vapply(
     ps_parents,
-    function(x) which(x == ps_children),
+    function(x) {
+      which(x == ps_children)
+    },
     integer(1)
   )
   parent_indices[[1]] <- NA_integer_
 
   child_parent <- cbind(ps, parent_indices)
 
-  list(ps = ps, parent_indices = parent_indices, to_delete = to_delete)
+  list(
+    parents = unname(parent_indices),
+    delete = unname(to_delete)
+  )
 }
 
-gwr_list_ <- function(graph) {
+gwr_list <- function(graph, calc_ps = TRUE) {
   names <- names(graph$hypotheses)
 
-  ps <- powerset_parents_(length(graph$hypotheses))
-
-  graphs <- vector("list", nrow(ps$ps))
-  graphs[[1]] <- graph
-
-  for (i in seq_len(nrow(ps$ps) - 1) + 1) {
-    parent <- graphs[[ps$parent_indices[[i]]]]
-
-    graphs[[i]] <- delete_node_fast(parent, ps$to_delete[[i]])
+  if (calc_ps) {
+    ps <- powerset_parents_fast(length(graph$hypotheses))
+  } else {
+    ps <- readRDS(
+      paste0(
+        "./perf-tests/data/",
+        length(graph$hypotheses),
+        ".rds"
+      )
+    )
   }
 
+  graphs <- vector("list", length(ps$parents))
+  graphs[[1]] <- graph
+
+  for (i in seq_len(length(ps$parents) - 1) + 1) {
+    parent <- graphs[[ps$parents[[i]]]]
+    del_index <- which(names[[ps$delete[[i]]]] == names(parent$hypotheses))
+
+    init_hypotheses <- parent$hypotheses
+    init_transitions <- parent$transitions
+
+    hypotheses <- parent$hypotheses
+    transitions <- parent$transitions
+
+    hyp_nums <- seq_along(hypotheses)[seq_along(hypotheses) != del_index]
+
+    for (hyp_num in hyp_nums) {
+      hypotheses[[hyp_num]] <-
+        init_hypotheses[[hyp_num]] +
+        init_hypotheses[[del_index]] * init_transitions[[del_index, hyp_num]]
+
+      denominator <- 1 - init_transitions[[hyp_num, del_index]] *
+        init_transitions[[del_index, hyp_num]]
+
+      for (end_num in hyp_nums) {
+        if (hyp_num == end_num || denominator <= 0) {
+          transitions[[hyp_num, end_num]] <- 0
+        } else {
+          transitions[[hyp_num, end_num]] <- (init_transitions[[hyp_num, end_num]] +
+            init_transitions[[hyp_num, del_index]] *
+              init_transitions[[del_index, end_num]]) / denominator
+        }
+      }
+    }
+
+    graphs[[i]] <- structure(
+      list(
+        hypotheses = hypotheses[-del_index],
+        transitions = as.matrix(transitions[-del_index, -del_index])
+      ),
+      class = "initial_graph"
+    )
+
+  }
+
+  # This could be eliminated by storing the weights separately on the first loop
+  # through
   wgts_mat <- structure(
     do.call(
       rbind,
-      lapply(graphs, function(graph) graph$hypotheses[names])
+      lapply(graphs, function(graph) {
+        graph$hypotheses[names]
+      })
     ),
     dimnames = list(1:(2^length(names) - 1), names)
   )
@@ -47,89 +135,7 @@ gwr_list_ <- function(graph) {
   cbind(wgts_mat_h, wgts_mat)
 }
 
-gwr_list <- function(graph, calc_ps = FALSE) {
-  if (calc_ps) {
-    ps_p <- powerset_parents(seq_along(graph$hypotheses))
-  } else {
-    ps_p <- readRDS(
-      paste0("./perf-tests/data/", length(graph$hypotheses), ".rds")
-    )
-  }
-
-  subgraphs <- ps_p
-
-  for (i in seq_along(ps_p)) {
-    if (is.null(ps_p[[i]])) {
-      subgraphs[[i]] <- graph
-    } else {
-      subgraphs[[i]] <- delete_node_fast(
-        subgraphs[[paste0(ps_p[[i]], collapse = "_")]],
-        match(
-          setdiff(
-            ps_p[[i]],
-            as.integer(strsplit(names(ps_p)[[i]], "_")[[1]])
-          ),
-          ps_p[[i]]
-        )
-      )
-    }
-  }
-
-  subgraphs
-}
-
-powerset_parents <- function(hyp_nums, last = 0, parent = NULL) {
-  # base case
-  is_single_node <- length(hyp_nums) == 1
-  last_is_bigger <- last > max(hyp_nums)
-
-  if (is_single_node || last_is_bigger) {
-    return(
-      structure(
-        list(parent = parent),
-        names = paste0(hyp_nums, collapse = "_")
-      )
-    )
-  }
-
-  # recursive step
-  children <- list()
-
-  for (hyp_num in hyp_nums[hyp_nums > last]) {
-    del_index <- match(hyp_num, hyp_nums)
-
-    children[[del_index]] <- powerset_parents(
-      hyp_nums[-del_index],
-      hyp_num,
-      hyp_nums
-    )
-  }
-
-  c(
-    structure(
-      list(parent = parent),
-      names = paste0(hyp_nums, collapse = "_")
-    ),
-    unlist(children, recursive = FALSE)
-  )
-}
-
-# powerset_parents can actually be pre-computed, and the lists saved. Then just
-# pick the right one for your graph size and index into it
-# There's probably some way to sequentially build up these index lists too, but
-# I can't think of it right now
-save_powerset_parents <- function(n_start, n_end) {
-  lapply(
-    n_start:n_end,
-    function(n) {
-      ps_p <- powerset_parents(seq_len(n))
-      saveRDS(ps_p, paste0("./perf-tests/data/", n, ".rds"))
-    }
-  )
-}
-
 # good for now - could convert to cpp at some point
-# prior solution by Dong/Spencer is faster as well, but uses more memory?
 delete_node_fast <- function(graph, del_index) {
   init_hypotheses <- graph$hypotheses
   init_transitions <- graph$transitions
@@ -137,7 +143,8 @@ delete_node_fast <- function(graph, del_index) {
   hypotheses <- graph$hypotheses
   transitions <- graph$transitions
 
-  hyp_nums <- seq_along(hypotheses)[seq_along(hypotheses) != del_index]
+  hyp_nums <-
+    seq_along(hypotheses)[seq_along(hypotheses) != del_index]
 
   for (hyp_num in hyp_nums) {
     hypotheses[[hyp_num]] <-
@@ -151,11 +158,9 @@ delete_node_fast <- function(graph, del_index) {
       if (hyp_num == end_num || denominator <= 0) {
         transitions[[hyp_num, end_num]] <- 0
       } else {
-        transitions[[hyp_num, end_num]] <- (
-          init_transitions[[hyp_num, end_num]] +
-            init_transitions[[hyp_num, del_index]] *
-            init_transitions[[del_index, end_num]]
-        ) / denominator
+        transitions[[hyp_num, end_num]] <- (init_transitions[[hyp_num, end_num]] +
+          init_transitions[[hyp_num, del_index]] *
+            init_transitions[[del_index, end_num]]) / denominator
       }
     }
   }
--- perf-tests/read_log.R
+++ perf-tests/read_log.R
@@ -6,21 +6,16 @@ library(ggplot2)
 marks_list <-
   fs::dir_ls(
     "./perf-tests/log/",
-    regexp = "bh[0-9]{1,2}_no-gmcp_2023-01-12 (21|22):(13|14|16|21|40).*"
+    regexp = "bh.*_no-gmcp_2023-01-31.*"
   ) |>
-  map(\(x) vroom(x, "\t", col_types = "nnnnnnnnnllll"))
+  map(\(x) vroom(x, "\t", col_types = "ncnnnnnnnnllll"))
 
-marks <- vec_rbind(!!!marks_list) |>
-  tibble::rownames_to_column() |>
-  dplyr::mutate(
-    rowname = (as.numeric(rowname) + 1) %% 2 + 1,
-    index = paste(rowname, expression)
-  )
+marks <- vec_rbind(!!!marks_list)
 
 marks
 
 gg_marks <- ggplot(marks) +
-  geom_point(aes(expression, `itr/sec`, colour = as.character(rowname)))
+  geom_point(aes(size, median, colour = expression))
 
 gg_marks
 
