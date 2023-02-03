5fa3f1a3a3b82ae8dbadffd5cb9342a38a051243
EeethB <ethanbrockmann@gmail.com>
2023-02-03 17:10:23 +0000

    Updates vignette analyzing the performance of various weight-generation methods

--- R/gw_original.R
+++ R/gw_original.R
@@ -1,6 +1,8 @@
 # This is a version of generate_weights_recursive() that is based on
 # update_graph() and is as simple as possible. It is rather slow, but may be
 # useful for testing, since it will be easier to make sure it is correct
+#' @export
+#' @rdname generate_weights
 gw_original <- function(graph) {
   n <- length(graph$hypotheses)
   ps_indices <- rev(expand.grid(rep(list(1:0), n))[-2^n, ])
