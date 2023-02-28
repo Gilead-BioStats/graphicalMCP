f738ad44081527a861c948e05929de0fc4d5f33c
EeethB <ethanbrockmann@gmail.com>
2023-02-28 04:50:21 +0000

    Cleans up some comments

--- R/graph_report.R
+++ R/graph_report.R
@@ -20,35 +20,19 @@ test_graph <- function(graph,
   graph_size <- length(graph$hypotheses)
   gw_size <- 2 ^ graph_size - 1
   num_groups <- length(groups)
+
   hyp_names <- names(graph$hypotheses)
   names(p) <- hyp_names
   if (!is.null(corr)) dimnames(corr) <- list(hyp_names, hyp_names)
 
   intersections <- generate_weights(graph)
   inter_h_vecs <- intersections[, seq_len(graph_size), drop = FALSE]
-  # inter_weights <-
-  #   intersections[, seq_len(graph_size) + graph_size, drop = FALSE]
   inter_small <- ifelse(
-    intersections[, seq_len(graph_size)],
+    inter_h_vecs,
     intersections[, seq_len(graph_size) + graph_size],
     NA_real_
   )
 
-  # Create group list
-  # groups_x_inters <- vector("list", length(groups) * gw_size)
-  # rep_groups <- rep(groups, gw_size)
-  # groups_list <- rep_groups
-  # groups_list_name <- rep(
-  #   lapply(groups, function(group) hyp_names[group]),
-  #   gw_size
-  # )
-
-  # Turns e.g. list(1, 2:3, 4) into list(1, 2:3, 4, 5, 6:7, 8,...57, 58:59, 60)
-  # for (i in seq_along(rep_groups)) {
-  #   groups_list[[i]] <- rep_groups[[i]] + ((i - 1) %/% num_groups) * graph_size
-  # }
-
-  # Initialize adj-p matrix
   p_adj <- matrix(
     NA_real_,
     nrow = gw_size,
@@ -59,9 +43,7 @@ test_graph <- function(graph,
     )
   )
 
-  # Loop
   for (i in seq_len(gw_size * num_groups)) {
-    # You still have to index into gw at some point
     row <- (i - 1) %/% num_groups + 1
     h <- inter_h_vecs[row, ]
     weights <- inter_small[row, ]
@@ -72,8 +54,7 @@ test_graph <- function(graph,
     test <- tests[[group_index]]
 
     group_in_subgraph <- group[as.logical(h[group])]
-    # Test each group - Adj-p with optional c
-    # Stick results into matrix
+
     p_adj[row, group_index] <- do.call(
       paste0("p_adjust_", test),
       list(
@@ -82,10 +63,8 @@ test_graph <- function(graph,
         corr = corr[group_in_subgraph, group_in_subgraph]
       )
     )
-
-  # End loop
   }
-  # rowMin for inter adj-p
+
   p_adj_inter <- apply(p_adj, 1, min) # NA should never exist, so leave them in
   test_inter <- p_adj_inter <= alpha
   p_adj_global <- apply(p_adj_inter * inter_h_vecs, 2, max)
@@ -109,8 +88,8 @@ test_graph <- function(graph,
         list(
           results = cbind(
             inter_small,
-            p_adj_grp = p_adj,
-            p_adj = p_adj_inter,
+            p_adj,
+            p_adj_inter,
             res = test_inter
           )
         )
