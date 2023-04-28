7040e8b1291057069b4e519d34182b9b2fea0e80
Ethan Brockmann <59264453+EeethB@users.noreply.github.com>
2023-04-28 00:07:05 -0400

    Add more graph test unit tests

--- tests/testthat/test-graph_report.R
+++ tests/testthat/test-graph_report.R
@@ -2,14 +2,19 @@ meta_test_graph <- function(...) {
   args <- list(...)
 
   valid_inputs <- list(
-    graph = par_gate,
-    p = p_val,
-    alpha = alpha_val,
-    groups = groups_val,
-    test_types = tests_val,
-    corr = corr_val,
-    verbose = verbose_val,
-    critical = critical_val
+    graph = simple_successive_1(),
+    p = c(.001, .02, .002, .03),
+    alpha = .025,
+    groups = list(1, 2, 3:4),
+    test_types = c("bonferroni", "simes", "parametric"),
+    corr = rbind(
+      c(NA, NA, NA, NA),
+      c(NA, NA, NA, NA),
+      c(NA, NA, 1,  0),
+      c(NA, NA, 0,  1)
+    ),
+    verbose = TRUE,
+    critical = TRUE
   )
 
   if (length(args) > 0) {
@@ -28,37 +33,31 @@ test_that("invalid test inputs throw errors", {
   par_gate <- simple_successive_1()
   par_gate_inval <- unclass(par_gate)
 
-  p_val <- c(.001, .02, .002, .03)
+  p_val <- c(.001, .01, .002, .03)
   p_inval1 <- as.character(p_val)
   p_inval2 <- c(-.001, .01, .002, 1.03)
   p_inval3 <- p_val[1:3]
 
-  alpha_val <- .025
   alpha_inval1 <- ".025"
   alpha_inval2 <- c(.025, .05)
   alpha_inval3 <- 1.1
 
-  tests_val <- c("bonferroni", "simes", "parametric")
   tests_inval1 <- c("bonferoni", "simes", "parametric")
   tests_inval2 <- c("bonferroni", "simes")
 
-  groups_val <- list(1, 2, 3:4)
   groups_inval1 <- list(1, 1, 3:4)
   groups_inval2 <- list(1, 3:4)
 
-  verbose_val <- TRUE
   verbose_inval <- c(TRUE, FALSE)
 
-  critical_val <- TRUE
   critical_inval <- 1
 
-  corr_val <- rbind(
+  corr_inval1 <- rbind(
     c(NA, NA, NA, NA),
     c(NA, NA, NA, NA),
     c(NA, NA, 1,  0),
     c(NA, NA, 0,  1)
   )
-  corr_inval1 <- corr_val
   corr_inval1[3, 4] <- .01
   corr_inval2 <- diag(2)
 
@@ -85,4 +84,104 @@ test_that("invalid test inputs throw errors", {
   expect_error(meta_test_graph(corr = corr_inval2))
 })
 
+test_that("adjusted p-values are capped at 1", {
+  expect_equal(
+    test_graph(random_graph(2), c(1, 1))$outputs$p_adj,
+    c(H1 = 1, H2 = 1)
+  )
+})
 
+test_that("Simes & parametric adjusted p-values are less than Bonferroni", {
+  rando <- random_graph(4)
+
+  expect_true(
+    all(
+      test_graph(rando, rep(.01, 4))$outputs$p_adj >=
+        test_graph(rando, rep(.01, 4), test_types = "s")$outputs$p_adj
+    )
+  )
+
+  expect_true(
+    all(
+      test_graph(rando, rep(.01, 4))$outputs$p_adj >=
+        test_graph(
+          rando,
+          rep(.01, 4),
+          test_types = "p",
+          corr = diag(4)
+        )$outputs$p_adj
+    )
+  )
+})
+
+test_that("verbose/critical output is only present when asked for", {
+  rando <- random_graph(6)
+
+  expect_null(test_graph(rando, rep(.01, 6))$details)
+  expect_null(test_graph(rando, rep(.01, 6))$critical)
+
+  expect_type(
+    test_graph(rando, rep(.01, 6), verbose = TRUE)$details$results,
+    "double"
+  )
+  expect_s3_class(
+    test_graph(rando, rep(.01, 6), critical = TRUE)$critical$results,
+    "data.frame"
+  )
+})
+
+test_that("check assertions in testing vignette", {
+  par_gate <- simple_successive_1(c("A1", "A2", "B1", "B2"))
+  pvals <- c(.024, .01, .026, .027)
+
+  expect_equal(
+    test_graph(par_gate, p = pvals, alpha = .05)$outputs,
+    list(
+      p_adj = c(.048, .02, .052, .052),
+      rejected = c(TRUE, TRUE, FALSE, FALSE)
+    ),
+    ignore_attr = TRUE
+  )
+
+  expect_equal(
+    test_graph(par_gate, p = pvals, alpha = .05, test_types = "s")$outputs,
+    list(
+      p_adj = c(.027, .02, .027, .027),
+      rejected = rep(TRUE, 4)
+    ),
+    ignore_attr = TRUE
+  )
+
+  corr1 <- matrix(nrow = 4, ncol = 4)
+  corr1[3:4, 3:4] <- .5
+  diag(corr1) <- 1
+
+  expect_equal(
+    test_graph(
+      par_gate,
+      pvals,
+      .05,
+      list(1:2, 3:4),
+      c("b", "p"),
+      corr1
+    )$outputs,
+    list(
+      p_adj = c(.048, .02, .048, .048),
+      rejected = rep(TRUE, 4)
+    ),
+    ignore_attr = TRUE
+  )
+
+  expect_equal(
+    test_graph(
+      par_gate,
+      pvals,
+      .05,
+      list(1, 2, 3, 4),
+      rep("p", 4),
+      corr3
+    )$outputs,
+    test_graph(par_gate, pvals, .05)$outputs
+  )
+
+})
