cff5cddf5bb17c9e01a19ec45de3997005341de9
EeethB <ethanbrockmann@gmail.com>
2023-05-18 21:00:20 +0000

    Begin adding citations

--- R/graph_report.R
+++ R/graph_report.R
@@ -33,6 +33,38 @@
 #' @seealso [test_graph_fast()], [bonferroni_sequential_cpp()]
 #' @export
 #'
+#' @references
+#'
+#' Bretz, F., Maurer, W., Brannath, W., and Posch, M. (2009). A graphical
+#' approach to sequentially rejective multiple test procedures. Statistics in
+#' Medicine, 28(4), 586–604. https://doi.org/10.1002/sim.3495
+#'
+#' Bretz, F., Maurer, W., and Hommel, G. (2011). Test and power considerations
+#' for multiple endpoint analyses using sequentially rejective graphical
+#' procedures. Statistics in Medicine, 30(13), 1489–1501.
+#' https://doi.org/10.1002/sim.3988
+#'
+#' Bretz, F., Posch, M., Glimm, E., Klinglmueller, F., Maurer, W., and Rohmeyer,
+#' K. (2011). Graphical approaches for multiple comparison procedures using
+#' weighted Bonferroni, Simes, or parametric tests. Biometrical Journal, 53(6),
+#' 894–913. https://doi.org/10.1002/bimj.201000239
+#'
+#' Lu, K. (2016). Graphical approaches using a Bonferroni mixture of weighted
+#' Simes tests. Statistics in Medicine, 35(22), 4041–4055.
+#' https://doi.org/10.1002/sim.6985
+#'
+#' Xi, D., Glimm, E., Maurer, W., and Bretz, F. (2017). A unified framework for
+#' weighted parametric multiple test procedures. Biometrical Journal, 59(5),
+#' 918–931. https://doi.org/10.1002/bimj.201600233
+#'
+#' Xi, D., and Bretz, F. (2019). Symmetric graphs for equally weighted tests,
+#' with application to the Hochberg procedure. Statistics in Medicine, 38(27),
+#' 5268–5282. https://doi.org/10.1002/sim.8375
+#'
+#' Rohmeyer K, Klinglmueller F (2020). _gMCP: Graph Based Multiple Test
+#' Procedures_. R package version 0.8-15,
+#' <https://cran.r-project.org/package=gMCP>.
+#'
 #' @examples
 #'
 #' hypotheses <- c(0.5, 0.5, 0, 0)
