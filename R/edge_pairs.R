4568d0043ca0a7f4c48e7f609292ded9aaa12a83
Ethan Brockmann <59264453+EeethB@users.noreply.github.com>
2023-05-19 16:36:58 -0500

    Add initial plotting generic :rocket:

--- R/edge_pairs.R
+++ R/edge_pairs.R
@@ -5,15 +5,10 @@ edge_pairs <- function(graph) {
 
   pair_nums <- which(pair_indices, arr.ind = TRUE, useNames = FALSE)
 
-  if (nrow(pair_nums) > 0) {
-    apply(
-      pair_nums,
-      1,
-      function(row) paste(g_names[[row[[1]]]], g_names[[row[[2]]]], sep = "|"),
-      simplify = FALSE
-    )
-  } else {
-    NULL
-  }
-
+  apply(
+    pair_nums,
+    1,
+    function(row) paste(g_names[[row[[1]]]], g_names[[row[[2]]]], sep = "|"),
+    simplify = FALSE
+  )
 }
