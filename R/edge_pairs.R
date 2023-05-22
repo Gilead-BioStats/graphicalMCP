88535119fa4658cad86bc115d1c42144e11e9306
EeethB <ethanbrockmann@gmail.com>
2023-05-22 21:22:41 +0000

    Add more plotting options

--- R/edge_pairs.R
+++ R/edge_pairs.R
@@ -5,10 +5,15 @@ edge_pairs <- function(graph) {
 
   pair_nums <- which(pair_indices, arr.ind = TRUE, useNames = FALSE)
 
-  apply(
-    pair_nums,
-    1,
-    function(row) paste(g_names[[row[[1]]]], g_names[[row[[2]]]], sep = "|"),
-    simplify = FALSE
-  )
+  if (nrow(pair_nums) > 0) {
+    apply(
+      pair_nums,
+      1,
+      function(row) paste(g_names[[row[[1]]]], g_names[[row[[2]]]], sep = "|"),
+      simplify = FALSE
+    )
+  } else {
+    NULL
+  }
+
 }
