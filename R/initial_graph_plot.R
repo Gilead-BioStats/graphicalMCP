83e3e1377548b9ed557d442c907d0eb5537bf91d
EeethB <ethanbrockmann@gmail.com>
2023-05-22 21:22:41 +0000

    Add more plotting options

--- R/initial_graph_plot.R
+++ R/initial_graph_plot.R
@@ -1,92 +1,101 @@
+#' S3 plot method for the class `initial_graph`
+#'
+#' @param graph
+#' @param layout
+#' @param groups
+#' @param groups_layout
+#' @param nrow
+#' @param ncol
+#' @param edge_curves
+#' @param precision
+#' @param epsilon
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
 plot.initial_graph <- function(graph,
-                               layout = igraph::layout.auto,
+                               layout = igraph::layout_nicely,
                                groups = list(graph$hypotheses),
                                groups_layout = NULL,
                                nrow = NULL,
                                ncol = NULL,
                                edge_curves = NULL,
                                precision = 4,
-                               epsilon = .001) {
+                               eps = .001,
+                               background_color = "white",
+                               ...) {
   graph_size <- length(graph$hypotheses)
   graph_names <- names(graph$hypotheses)
+  graph_seq <- seq_along(graph$hypotheses)
 
-  # structure igraph object ----------------------------------------------------
-  all_vert_cross <- rev(expand.grid(
-    end = seq_along(graph$hypotheses),
-    start = seq_along(graph$hypotheses)
-  ))
+  graph_igraph <- as_igraph(graph)
 
-  which_edge <- apply(
-    all_vert_cross,
-    1,
-    function(row) graph$transitions[row[[1]], row[[2]]]
-  ) != 0
-
-  df_edges <- all_vert_cross[which_edge, ]
-
-  graph_igraph <- make_directed_graph(t(df_edges))
-  graph_igraph <- set_vertex_attr(graph_igraph, "name", value = graph_names)
-  graph_igraph <- set_vertex_attr(graph_igraph, "label", value = graph_names)
+  v_attr <- vertex_attr(graph_igraph)
+  e_attr <- edge_attr(graph_igraph)
 
   # make labels ----------------------------------------------------------------
-  vert_labels <- paste(
-    graph_names,
-    round(graph$hypotheses, precision),
-    sep = "\n"
-  )
-  edge_labels <- round(
-    t(graph$transitions)[t(graph$transitions) > 0],
-    precision
-  )
+  v_labels <- paste(v_attr$name, round(v_attr$weight, precision), sep = "\n")
 
-  edge_labels[edge_labels <= epsilon & edge_labels != 0] <- "e"
-  edge_labels[edge_labels >= 1 - epsilon & edge_labels != 1] <- "1 - e"
+  # very small edges should display as epsilon
+  edge_labels <- e_attr$weight
+
+  near_0 <- edge_labels <= eps & edge_labels != 0
+  near_1 <- edge_labels >= 1 - eps & edge_labels != 1
+
+  edge_labels[!near_0 & !near_1] <-
+    round(edge_labels[!near_0 & !near_1], precision)
+  edge_labels[near_0] <- expression(epsilon)
+  edge_labels[near_1] <- expression(1 - epsilon)
 
   # set curves -----------------------------------------------------------------
-  curve <- rep(0, length(E(graph_igraph)))
-  names(curve) <- attr(E(graph_igraph), "vnames")
+  curve <- rep(0, length(igraph::E(graph_igraph)))
+  names(curve) <- attr(igraph::E(graph_igraph), "vnames")
 
   # vertex pairs connected in both directions should get a small default so
   # their edges don't overlap each other
-  edge_pair_locs <- attr(E(graph_igraph), "vnames") %in% edge_pairs(graph)
+  edge_pair_locs <- attr(igraph::E(graph_igraph), "vnames") %in% edge_pairs(graph)
   curve[edge_pair_locs] <- .25
 
   curve[names(edge_curves)] <- edge_curves
 
   # set layout -----------------------------------------------------------------
   if (!is.function(layout)) {
-    if (layout == "grid") {
-      if (is.null(nrow) && is.null(ncol)) {
-        nrow <- ceiling(sqrt(graph_size))
-        ncol <- ncol
-      } else if(is.null(nrow)) {
-        nrow <- ceiling(graph_size / ncol)
-      } else if(is.null(ncol)) {
-        nrow <- ceiling(graph_size / nrow)
+    if (!is.matrix(layout)) {
+      if (layout == "grid") {
+        if (is.null(nrow) && is.null(ncol)) {
+          nrow <- ceiling(sqrt(graph_size))
+          ncol <- nrow
+        } else if(is.null(nrow)) {
+          nrow <- ceiling(graph_size / ncol)
+        } else if(is.null(ncol)) {
+          ncol <- ceiling(graph_size / nrow)
+        }
+
+        # [] removes extras when grid is not full
+        layout <- cbind(
+          rep(seq_len(ncol), nrow)[graph_seq],
+          vapply(rev(seq_len(nrow)), rep, integer(ncol), ncol)[graph_seq]
+        )
       }
-
-      layout <- matrix(
-        nrow = graph_size,
-        ncol = 2,
-        dimnames = list(graph_names, c("x", "y"))
-      )
-
-      # [] removes extras
-      layout[, "x"] <- rep(seq_len(ncol), nrow)[seq_len(graph_size)]
-      layout[, "y"] <- do.call(
-        c,
-        lapply(rev(seq_len(nrow)), rep, ncol)
-      )[seq_len(graph_size)]
     }
   }
 
+  par(bg = background_color)
+
   # draw! ----------------------------------------------------------------------
-  plot(
+  igraph::plot.igraph(
     graph_igraph,
+    ...,
 
     layout = layout,
 
-    # vertex.label = vert_labels,
+    vertex.color = "#e8c2ff",
+    # vertex.frame.color = "#e8c2ff",
+    vertex.label = v_labels,
+    vertex.label.color = "black",
+    vertex.size = 35,
 
     edge.label = edge_labels,
     edge.curved = curve,
