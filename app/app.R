eafa12eb3d2a91c6e44c24c0ee0fe25de265cce4
EeethB <ethanbrockmann@gmail.com>
2023-10-10 16:30:15 +0000

    Add p-values

--- app/app.R
+++ app/app.R
@@ -4,66 +4,99 @@ library(graphicalMCP)
 
 ui <- fluidPage(
 
-  # Application title
-  titlePanel("Graphical Multiple Comparison Procedures"),
-
   fluidRow(
-    column(6, plotOutput("graphPlot")),
+
+    titlePanel("Initial graph"),
+
+    column(5, plotOutput("plot_initial_graph")),
+
     column(
-      6,
-      selectInput("layout", "Graph layout", c("grid", "layout_nicely"))
+      7,
+      numericInput(
+        "num_hyps",
+        "Number of hypotheses",
+        value = 4,
+        min = 1,
+        max = 50,
+        step = 1
+      ),
+      shinyMatrix::matrixInput(
+        "hypotheses",
+        "Hypothesis weights",
+        value = structure(
+          matrix(0, 1, 1),
+          dimnames = list(NULL, "H1")
+        ),
+        class = "numeric",
+        rows = list(
+          n = 1,
+          names = FALSE
+        ),
+        cols = list(
+          n = 1,
+          names = TRUE
+        )
+      ),
+      shinyMatrix::matrixInput(
+        "transitions",
+        "Transition weights",
+        value = structure(
+          matrix(0, 1, 1),
+          dimnames = list("H1", "H1")
+        ),
+        class = "numeric",
+        rows = list(
+          n = 1,
+          names = TRUE
+        ),
+        cols = list(
+          n = 1,
+          names = TRUE
+        )
+      )
     )
   ),
 
   hr(),
 
   fluidRow(
+
+    titlePanel("Test the graph"),
+
+    column(5, plotOutput("plot_updated_graph")),
+
     column(
-        6,
-        numericInput(
-            "num_hyps",
-            "Number of hypotheses",
-            value = 4,
-            min = 1,
-            max = 50,
-            step = 1
+      7,
+
+      shinyMatrix::matrixInput(
+        "p",
+        "P-values",
+        value = structure(
+          matrix(1, 1, 1),
+          dimnames = list(NULL, "H1")
         ),
-        matrixInput(
-            "hypotheses",
-            "Hypothesis weights",
-            value = structure(
-                matrix(0, 1, 1),
-                dimnames = list("weights", "H1")
-            ),
-            class = "numeric",
-            rows = list(
-                n = 1,
-                names = TRUE
-            ),
-            cols = list(
-                n = 1,
-                names = TRUE
-            )
+        class = "numeric",
+        rows = list(
+          n = 1,
+          names = FALSE
         ),
-        matrixInput(
-            "transitions",
-            "Transition weights",
-            value = structure(
-                matrix(0, 1, 1),
-                dimnames = list("H1", "H1")
-            ),
-            class = "numeric",
-            rows = list(
-                n = 1,
-                names = TRUE
-            ),
-            cols = list(
-                n = 1,
-                names = TRUE
-            )
+        cols = list(
+          n = 1,
+          names = TRUE
         )
+      ),
+
+      selectInput(
+        "test_type",
+        "Test type",
+        c("Bonferroni", "Parametric", "Simes"),
+        selected = "Bonferroni"
+      )
+
     )
+
   )
+
 )
 
 # Define server logic required to draw a histogram
@@ -93,9 +126,9 @@ server <- function(input, output, session) {
     )
 
     dimnames(new_hypotheses) <-
-      list("wts", paste0("H", global$seq_hyps))
+      list(NULL, paste0("H", global$seq_hyps))
 
-    updateMatrixInput(session, "hypotheses", new_hypotheses)
+    shinyMatrix::updateMatrixInput(session, "hypotheses", new_hypotheses)
   })
 
   # Update transition weights
@@ -120,28 +153,49 @@ server <- function(input, output, session) {
       paste0("H", global$seq_hyps)
     )
 
-    updateMatrixInput(session, "transitions", new_transitions)
+    shinyMatrix::updateMatrixInput(session, "transitions", new_transitions)
   })
 
-  output$graphPlot <- renderPlot({
+  # Update p-values
+  observe({
+    if (global$num_hyp_diff >= 0) {
+      new_p <- c(input$p, rep(1, global$num_hyp_diff))
+    } else {
+      new_p <- input$p[global$seq_hyps]
+    }
+
+    new_p <- matrix(
+      new_p,
+      nrow = 1,
+      ncol = input$num_hyps
+    )
+
+    dimnames(new_p) <-
+      list(NULL, paste0("H", global$seq_hyps))
+
+    shinyMatrix::updateMatrixInput(session, "p", new_p)
+  })
+
+  initial_graph <- reactive({
     clean_hypotheses <- input$hypotheses
     clean_hypotheses[is.na(clean_hypotheses)] <- 0
 
     clean_transitions <- input$transitions
     clean_transitions[is.na(clean_transitions)] <- 0
 
-    graph <- graph_create(
-      clean_hypotheses,
-      clean_transitions
-    )
+    graph <- graph_create(clean_hypotheses, clean_transitions)
+  })
 
-    if (input$layout != "grid") {
-      layout <- as.function(input$layout)
-    } else {
-      layout <- input$layout
-    }
+  output$plot_initial_graph <- renderPlot({
+    plot(initial_graph())
+  })
 
-    plot(graph, layout = layout)
+  test_results <- reactive({
+    graph_test_closure(initial_graph(), input$p, test_types = input$test_type)
+  })
+
+  output$plot_updated_graph <- renderPlot({
+    plot(graph_update(initial_graph(), test_results()$outputs$rejected))
   })
 }
 
